354ba792652ab6458ecef954e3504f82151cde1c
Hossain Khan <hello@hossain.dev>
2026-09-24 21:17:34 -0400

    chore: prepare release 0.38.0

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.hossain.highlight.sample"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "0.37.1"
+        versionCode = 46
+        versionName = "0.38.0"
         buildConfigField("String", "LIB_VERSION_NAME", "\"${project.findProperty("VERSION_NAME") ?: versionName}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
